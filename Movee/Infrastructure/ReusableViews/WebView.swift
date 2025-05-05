6ebe16a5abc21be48d5d149c5daad159fda0b647
Oleh Zahoriansky <olehzahor@gmail.com>
2025-05-05 15:11:13 +0300

    basic youtube player preps

---
+++ Movee/Infrastructure/ReusableViews/WebView.swift
@@ -0,0 +1,77 @@
+//
+//  WebView.swift
+//  Movee
+//
+//  Created by user on 5/5/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    typealias LoadingStateUpdater = (URL?, Bool) -> Void
+    
+    enum Content {
+        case url(URL)
+        case html(String)
+    }
+
+    let content: Content
+    private let onLoadingStateChanged: LoadingStateUpdater?
+
+    init(_ content: Content,
+         onLoadingStateChanged: LoadingStateUpdater? = nil)
+    {
+        self.content = content
+        self.onLoadingStateChanged = onLoadingStateChanged
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        config.mediaTypesRequiringUserActionForPlayback = []
+        config.allowsInlineMediaPlayback = true           // important!
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.navigationDelegate = context.coordinator
+        return webView
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        guard !context.coordinator.hasLoadedInitialContent else { return }
+        switch content {
+        case .url(let url):
+            print("Loading url: \(url)")
+            webView.load(URLRequest(url: url))
+        case .html(let html):
+            webView.loadHTMLString(html, baseURL: nil)
+        }
+        context.coordinator.hasLoadedInitialContent = true
+    }
+
+    func makeCoordinator() -> Coordinator { .init(self) }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        let parent: WebView
+        private var lastLoadingState: Bool = false
+        var hasLoadedInitialContent: Bool = false
+        init(_ parent: WebView) { self.parent = parent }
+        
+        func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+            if lastLoadingState {
+                parent.onLoadingStateChanged?(webView.url, true)
+                lastLoadingState = false
+            }
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            if !lastLoadingState {
+                parent.onLoadingStateChanged?(webView.url, false)
+                lastLoadingState = true
+            }
+        }
+    }
+
+    /// Fluent API
+    func onLoadingStateChanged(_ handler: @escaping LoadingStateUpdater) -> WebView {
+        WebView(content, onLoadingStateChanged: handler)
+    }
+}
