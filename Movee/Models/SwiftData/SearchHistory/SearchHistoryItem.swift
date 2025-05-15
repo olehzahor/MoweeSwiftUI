4fd998f1aa6375edbc3033c0e399e6784056a9d2
Oleh Zahoriansky <olehzahor@gmail.com>
2025-05-15 21:02:27 +0300

    implemented search history

---
+++ Movee/Models/SwiftData/SearchHistory/SearchHistoryItem.swift
@@ -0,0 +1,20 @@
+//
+//  SearchHistoryItem.swift
+//  Movee
+//
+//  Created by user on 5/15/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class SearchHistoryItem: Sendable {
+    var media: StoredMedia
+    var added: Date
+    
+    init(media: StoredMedia) {
+        self.media = media
+        self.added = Date()
+    }
+}
