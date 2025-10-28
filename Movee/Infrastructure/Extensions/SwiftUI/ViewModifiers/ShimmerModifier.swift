0492f615f5e69ed594201dd730954d0c29452420
Oleh Zahoriansky <olehzahor@gmail.com>
2025-10-28 18:17:41 +0200

    improved shimmering

--- Movee/Infrastructure/Extensions/SwiftUI/ViewModifiers/ShimmerModifier.swift
+++ Movee/Infrastructure/Extensions/SwiftUI/ViewModifiers/ShimmerModifier.swift
@@ -2,20 +2,18 @@ import SwiftUI
 
 struct ShimmerModifier: ViewModifier {
     @State private var overlayOpacity: Double = 0.5
-
+    
     func body(content: Content) -> some View {
         content
-            .redacted(reason: .placeholder)
-            .opacity(overlayOpacity)
-//            .overlay(
-//                Rectangle()
-//                    .blendMode(.sourceAtop)
-//                    .foregroundColor(Color(.systemBackground))
-//                    .opacity(overlayOpacity)
-//            )
+            .overlay(
+                Color(.systemBackground)
+                    .blendMode(.sourceAtop)
+                    .opacity(overlayOpacity)
+            )
+            .compositingGroup()
             .onAppear {
                 withAnimation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
-                    overlayOpacity = 1//0.5
+                    overlayOpacity = 0
                 }
             }
     }
