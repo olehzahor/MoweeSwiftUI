6029c0570f3de11a9a653fc894ca8a8006e513dc
Oleh Zahoriansky <olehzahor@gmail.com>
2025-11-13 21:01:10 +0200

    cleanup

--- Movee/Scenes/MediasList/MediasListView.swift
+++ Movee/Scenes/MediasList/MediasListView.swift
@@ -1,163 +1,163 @@
+////
+////  MediasListView.swift
+////  Movee
+////
+////  Created by user on 4/7/25.
+////
 //
-//  MediasListView.swift
-//  Movee
+//import SwiftUI
+//import Combine
 //
-//  Created by user on 4/7/25.
 //
-
-import SwiftUI
-import Combine
-
-
-/*
- struct NewMediasListView: View {
-     @StateObject var viewModel: NewMediasListViewModel
-
-     private var title: String {
-         viewModel.section.fullTitle ?? viewModel.section.title
-     }
-     
-     private var medias: [Media] {
-         if !viewModel.medias.isEmpty {
-             return viewModel.medias
-         } else {
-             return Array(repeating: .placeholder, count: 10)
-         }
-     }
-     
-     private func setupRowView(_ media: Media) -> AnyView {
-         var view: any View = MediaRowView(data: .init(media: media)).onAppear {
-             if viewModel.isLastLoaded(media: media) {
-                 viewModel.fetchMedias()
-             }
-         }
-         
-         if media == .placeholder {
-             view = view.redacted(reason: .placeholder).shimmering()
-             return AnyView(view)
-         } else {
-             return AnyView(
-                 NavigationLink {
-                     MediaDetailsView(media: media)
-                 } label: {
-                     AnyView(view)
-                 }
-             )
-         }
-     }
-     
-     var body: some View {
-         NavigationStack {
-             Group {
-                 if viewModel.isLoaded, viewModel.medias.isEmpty,
-                    let placeholder = viewModel.section.placeholder {
-                     VStack(alignment: .center, spacing: 16) {
-                         Text(placeholder.title)
-                             .textStyle(.mediumTitle)
-                         if let subtitle = placeholder.subtitle {
-                             Text(subtitle)
-                                 .textStyle(.mediumSubtitle)
-                         }
-                     }.padding(.horizontal)
-                 } else {
-                     List(Array(medias.enumerated()), id: \.0) { _, media in
-                         ZStack {
-                             setupRowView(media)
-                         }
-                         .listRowSeparator(.hidden)
-                     }
-                     .listStyle(.plain)
-                     .scrollIndicators(.hidden)
-                     .onAppear {
-                         viewModel.fetchMedias()
-                     }
-                 }
-             }
-             .navigationTitle(title)
-             .navigationBarTitleDisplayMode(title == "Watchlist" ? .large : .inline)
-         }
-     }
-     
-     init(section: NewMediasSection) {
-         _viewModel = StateObject(wrappedValue: NewMediasListViewModel(section: section))
-     }
- }
-
- */
-
-struct MediasListView: View {
-    @StateObject var viewModel: MediasListViewModel
-
-    private var title: String {
-        viewModel.section.fullTitle ?? viewModel.section.title
-    }
-    
-    private var medias: [Media] {
-        if !viewModel.medias.isEmpty {
-            return viewModel.medias
-        } else {
-            return Array(repeating: .placeholder, count: 10)
-        }
-    }
-    
-    private func setupRowView(_ media: Media) -> AnyView {
-        var view: any View = MediaRowView(data: .init(media: media)).onAppear {
-            if viewModel.isLastLoaded(media: media) {
-                viewModel.fetchMedias()
-            }
-        }
-        
-        if media == .placeholder {
-            view = view.redacted(reason: .placeholder).shimmering()
-            return AnyView(view)
-        } else {
-            return AnyView(
-                NavigationLink {
-                    MediaDetailsView(media: media)
-                } label: {
-                    AnyView(view)
-                }
-            )
-        }
-    }
-    
-    var body: some View {
-        NavigationStack {
-            Group {
-                if viewModel.isLoaded, viewModel.medias.isEmpty,
-                   let placeholder = viewModel.section.placeholder {
-                    VStack(alignment: .center, spacing: 16) {
-                        Text(placeholder.title)
-                            .textStyle(.mediumTitle)
-                        if let subtitle = placeholder.subtitle {
-                            Text(subtitle)
-                                .textStyle(.mediumSubtitle)
-                        }
-                    }.padding(.horizontal)
-                } else {
-                    List(Array(medias.enumerated()), id: \.0) { _, media in
-                        ZStack {
-                            setupRowView(media)
-                        }
-                        .listRowSeparator(.hidden)
-                    }
-                    .listStyle(.plain)
-                    .scrollIndicators(.hidden)
-                    .onAppear {
-                        viewModel.fetchMedias()
-                    }
-                }
-            }
-            .navigationTitle(title)
-            .navigationBarTitleDisplayMode(title == "Watchlist" ? .large : .inline)
-        }
-    }
-    
-    init(section: MediasSection) {
-        _viewModel = StateObject(wrappedValue: MediasListViewModel(section: section))
-    }
-}
-
-#Preview {
-    MediasListView(section: [MediasSection].homePageSections.first!)
-}
+///*
+// struct NewMediasListView: View {
+//     @StateObject var viewModel: NewMediasListViewModel
+//
+//     private var title: String {
+//         viewModel.section.fullTitle ?? viewModel.section.title
+//     }
+//     
+//     private var medias: [Media] {
+//         if !viewModel.medias.isEmpty {
+//             return viewModel.medias
+//         } else {
+//             return Array(repeating: .placeholder, count: 10)
+//         }
+//     }
+//     
+//     private func setupRowView(_ media: Media) -> AnyView {
+//         var view: any View = MediaRowView(data: .init(media: media)).onAppear {
+//             if viewModel.isLastLoaded(media: media) {
+//                 viewModel.fetchMedias()
+//             }
+//         }
+//         
+//         if media == .placeholder {
+//             view = view.redacted(reason: .placeholder).shimmering()
+//             return AnyView(view)
+//         } else {
+//             return AnyView(
+//                 NavigationLink {
+//                     MediaDetailsView(media: media)
+//                 } label: {
+//                     AnyView(view)
+//                 }
+//             )
+//         }
+//     }
+//     
+//     var body: some View {
+//         NavigationStack {
+//             Group {
+//                 if viewModel.isLoaded, viewModel.medias.isEmpty,
+//                    let placeholder = viewModel.section.placeholder {
+//                     VStack(alignment: .center, spacing: 16) {
+//                         Text(placeholder.title)
+//                             .textStyle(.mediumTitle)
+//                         if let subtitle = placeholder.subtitle {
+//                             Text(subtitle)
+//                                 .textStyle(.mediumSubtitle)
+//                         }
+//                     }.padding(.horizontal)
+//                 } else {
+//                     List(Array(medias.enumerated()), id: \.0) { _, media in
+//                         ZStack {
+//                             setupRowView(media)
+//                         }
+//                         .listRowSeparator(.hidden)
+//                     }
+//                     .listStyle(.plain)
+//                     .scrollIndicators(.hidden)
+//                     .onAppear {
+//                         viewModel.fetchMedias()
+//                     }
+//                 }
+//             }
+//             .navigationTitle(title)
+//             .navigationBarTitleDisplayMode(title == "Watchlist" ? .large : .inline)
+//         }
+//     }
+//     
+//     init(section: NewMediasSection) {
+//         _viewModel = StateObject(wrappedValue: NewMediasListViewModel(section: section))
+//     }
+// }
+//
+// */
+//
+//struct MediasListView: View {
+//    @StateObject var viewModel: MediasListViewModel
+//
+//    private var title: String {
+//        viewModel.section.fullTitle ?? viewModel.section.title
+//    }
+//    
+//    private var medias: [Media] {
+//        if !viewModel.medias.isEmpty {
+//            return viewModel.medias
+//        } else {
+//            return Array(repeating: .placeholder, count: 10)
+//        }
+//    }
+//    
+//    private func setupRowView(_ media: Media) -> AnyView {
+//        var view: any View = MediaRowView(data: .init(media: media)).onAppear {
+//            if viewModel.isLastLoaded(media: media) {
+//                viewModel.fetchMedias()
+//            }
+//        }
+//        
+//        if media == .placeholder {
+//            view = view.redacted(reason: .placeholder).shimmering()
+//            return AnyView(view)
+//        } else {
+//            return AnyView(
+//                NavigationLink {
+//                    MediaDetailsView(media: media)
+//                } label: {
+//                    AnyView(view)
+//                }
+//            )
+//        }
+//    }
+//    
+//    var body: some View {
+//        NavigationStack {
+//            Group {
+//                if viewModel.isLoaded, viewModel.medias.isEmpty,
+//                   let placeholder = viewModel.section.placeholder {
+//                    VStack(alignment: .center, spacing: 16) {
+//                        Text(placeholder.title)
+//                            .textStyle(.mediumTitle)
+//                        if let subtitle = placeholder.subtitle {
+//                            Text(subtitle)
+//                                .textStyle(.mediumSubtitle)
+//                        }
+//                    }.padding(.horizontal)
+//                } else {
+//                    List(Array(medias.enumerated()), id: \.0) { _, media in
+//                        ZStack {
+//                            setupRowView(media)
+//                        }
+//                        .listRowSeparator(.hidden)
+//                    }
+//                    .listStyle(.plain)
+//                    .scrollIndicators(.hidden)
+//                    .onAppear {
+//                        viewModel.fetchMedias()
+//                    }
+//                }
+//            }
+//            .navigationTitle(title)
+//            .navigationBarTitleDisplayMode(title == "Watchlist" ? .large : .inline)
+//        }
+//    }
+//    
+//    init(section: MediasSection) {
+//        _viewModel = StateObject(wrappedValue: MediasListViewModel(section: section))
+//    }
+//}
+//
+//#Preview {
+//    MediasListView(section: [MediasSection].homePageSections.first!)
+//}
